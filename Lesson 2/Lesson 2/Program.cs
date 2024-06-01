e6079d4531272ae9b31a58272168873ee92f0512
Ozodbek <abdurahmonovo961@gmail.com>
2024-06-01 21:29:54 +0500

    Homework 2

---
+++ Lesson 2/Lesson 2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string address;
+        bool ismale;
+        int age;
+        
+        Console.WriteLine(" Fullname ");
+        string Fullname = Console.ReadLine();
+
+        Console.WriteLine("My name is " + Fullname);
+
+
+    }
+}
---
+++ Lsson 2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lsson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Mening yoshim " + num + "da");
+
+        }
+    }
+}
